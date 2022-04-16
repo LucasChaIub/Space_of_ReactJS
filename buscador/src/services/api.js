0613053d52e814a57ff7324e75098426369ad141
Lucas Chalub <lucas.alves.chalub@gmail.com>
2022-04-16 19:09:32 -0300

    Concluído

---
+++ buscador/src/services/api.js
@@ -0,0 +1,10 @@
+/* eslint-disable no-unused-vars */
+import axios from "axios";
+
+// 01310930/json/
+
+const api = axios.create({
+    baseURL: 'https://viacep.com.br/ws/'    
+});
+
+export default api;
